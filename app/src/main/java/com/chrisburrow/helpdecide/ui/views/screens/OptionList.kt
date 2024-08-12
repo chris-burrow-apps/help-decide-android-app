d2f72d8e675015e3170cbb4e004a34baebb8f91f
Chris Burrow <chris173972@gmail.com>
2024-08-12 18:26:56 +0100

    Splitting class for row views to make them less cluttered and more segmented

--- app/src/main/java/com/chrisburrow/helpdecide/ui/views/screens/OptionList.kt
+++ app/src/main/java/com/chrisburrow/helpdecide/ui/views/screens/OptionList.kt
@@ -1,30 +1,17 @@
 package com.chrisburrow.helpdecide.ui.views.screens
 
-import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.chrisburrow.helpdecide.ui.ThemePreviews
 import com.chrisburrow.helpdecide.ui.theme.HelpDecideTheme
-import com.chrisburrow.helpdecide.ui.views.DismissBackground
 import com.chrisburrow.helpdecide.utils.OptionObject
 
 class OptionListTags {
@@ -36,6 +23,7 @@ class OptionListTags {
         const val TEXT_TAG = "TextOption"
     }
 }
+
 @Composable
 fun OptionList(
     modifier: Modifier = Modifier,
@@ -62,69 +50,6 @@ fun OptionList(
                 )
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun OptionRowView(
-    modifier: Modifier = Modifier,
-    position: Int,
-    option: OptionObject,
-    removeOption: (OptionObject) -> Unit
-) {
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .testTag(OptionListTags.BASE_VIEW)
-    ) {
-
-        val context = LocalContext.current
-        val dismissState = rememberSwipeToDismissBoxState(
-            confirmValueChange = {
-                when(it) {
-                    SwipeToDismissBoxValue.Settled -> return@rememberSwipeToDismissBoxState false
-                    else -> {
-                        removeOption(option)
-                        Toast.makeText(context, "Option removed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                return@rememberSwipeToDismissBoxState true
-            },
-            positionalThreshold = { it * .25f }
-        )
-        SwipeToDismissBox(
-            state = dismissState,
-            modifier = modifier.testTag(OptionListTags.ROW_VIEW + position),
-            backgroundContent = { DismissBackground(dismissState)},
-            content = {
-                OptionTextView(position = position, option = option)
-            }
-        )
-    }
-}
-
-
-@Composable
-fun OptionTextView(position: Int, option: OptionObject) {
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Text(
-            modifier = Modifier
-                .testTag("${OptionListTags.TEXT_TAG}$position")
-                .fillMaxWidth()
-                .padding(15.dp)
-                .weight(1.0f),
-            color = MaterialTheme.colorScheme.primary,
-            text = option.text
-        )
     }
 }
 
---
+++ app/src/main/java/com/chrisburrow/helpdecide/ui/views/screens/OptionRowView.kt
@@ -0,0 +1,119 @@
+package com.chrisburrow.helpdecide.ui.views.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxState
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import com.chrisburrow.helpdecide.utils.OptionObject
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OptionRowView(
+    modifier: Modifier = Modifier,
+    position: Int,
+    option: OptionObject,
+    removeOption: (OptionObject) -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .testTag(OptionListTags.BASE_VIEW)
+    ) {
+
+        val context = LocalContext.current
+        val dismissState = rememberSwipeToDismissBoxState(
+            confirmValueChange = {
+                when(it) {
+                    SwipeToDismissBoxValue.Settled -> return@rememberSwipeToDismissBoxState false
+                    else -> {
+                        removeOption(option)
+                        Toast.makeText(context, "Option removed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                return@rememberSwipeToDismissBoxState true
+            },
+            positionalThreshold = { it * .25f }
+        )
+        SwipeToDismissBox(
+            state = dismissState,
+            modifier = modifier.testTag(OptionListTags.ROW_VIEW + position),
+            backgroundContent = { DismissBackground(dismissState)},
+            content = {
+                OptionTextView(position = position, option = option)
+            }
+        )
+    }
+}
+
+
+@Composable
+fun OptionTextView(position: Int, option: OptionObject) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            modifier = Modifier
+                .testTag("${OptionListTags.TEXT_TAG}$position")
+                .fillMaxWidth()
+                .padding(15.dp)
+                .weight(1.0f),
+            color = MaterialTheme.colorScheme.primary,
+            text = option.text
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DismissBackground(dismissState: SwipeToDismissBoxState) {
+    val color = when (dismissState.dismissDirection) {
+        SwipeToDismissBoxValue.Settled -> Color.Transparent
+        else -> Color(0xFFFF1744)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color)
+            .padding(12.dp, 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            Icons.Default.Delete,
+            contentDescription = "delete"
+        )
+        Spacer(modifier = Modifier)
+        Icon(
+            Icons.Default.Delete,
+            contentDescription = "delete"
+        )
+    }
+}
